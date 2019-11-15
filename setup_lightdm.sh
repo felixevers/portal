98b37e2e69c07e62b663e5f0ea648381d13fb247
SpartanerSpaten <espriworkemai@gmail.com>
2019-11-15 12:27:39 +0000

    fixed stupid bugs

--- setup_lightdm.sh
+++ setup_lightdm.sh
@@ -1,4 +1,6 @@
 
+# exit when any command fails
+set -e
 
 clear
 echo "--------------------------------------------------"
@@ -10,25 +12,48 @@ if type "pacman" > /dev/null 2>&1; then
   DISTRO="ARCH"
 elif type "apt" > /dev/null 2>&1; then
   DISTRO="DEBIAN"
+else
+  echo "Not supported distribution"
+  exit 1
+fi
 
 echo "You are running GNU / LINUX - $DISTRO"
 
-DOCKERIMAGE = $1
+echo "Do you want to run the default image (y/N)"
+read -n 1 INPUT ; echo; echo
 
-if [$DOCKERIMAGE == ""]; then
+DOCKERIMAGE=false
+if [ $INPUT == "y" ] || [ $INPUT == "Y" ]; then
+  DOCKERIMAGE="use-to/portal"
+  docker pull "$DOCKERIMAGE"
+else
+  #docker images
+  echo "please enter the requested docker image:"
+  read  INPUT; echo; echo
+  DOCKERIMAGE=$INPUT
+fi
+
+if [ $DOCKERIMAGE == "" ]; then
   echo "No valid docker image given ... exiting"
-  return 1
-
-elif [docker images | grep $DOCKERIMAGE == ""]; then
+  exit 1
+elif [[ "$(docker images -q $DOCKERIMAGE 2> /dev/null)" == "" ]]; then
   echo "Could not find this docker image: $DOCKERIMAGE ... exiting"
-  return 1
+  exit 1
+else
+  echo echo "Found docker image"
+fi
 
 echo "Installing packages ..."
 
-if [$DISTRO = "ARCH"]; then
+if [ $DISTRO = "ARCH" ]; then
+  pacman -Sy
   pacman -S lightdm  lightdm-webkit2-greeter
-elif [$DISTRO = "DEBIAN"]; then
+elif [ $DISTRO = "DEBIAN" ]; then
   apt-get install lightdm lightdm-webkit2-greeter lightdm-gtk-greeter lightdm-webkit-theme-aether
+else
+  echo "Could not find apt or pacman this distribution or package manager is not supported"
+  exit 1
+fi
 
 echo "Installing scripts ..."
 
@@ -45,8 +70,3 @@ echo "session-setup-script=/etc/portal/start_script.sh" >> /etc/lightdm/lightdm.
 
 echo "Enables Lightdm daemon ..."
 systemctl enable lightdm.service
-
-
-
-
-
